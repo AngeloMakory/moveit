658ab67911f6dbcc40149ce0bd1e8a580ebf9b24
Angelo Kizito Makory <angelomakory@gmail.com>
2022-11-29 16:14:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/moveit/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.example.moveit
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val loginButton = view.findViewById<Button>(R.id.loginbutton)
+        loginButton.setOnClickListener{
+            findNavController().navigate(R.id.action_HomeFragment_to_loginFragment)
+        }
+
+//        val homeButton = view.findViewById<Button>(R.id.homebutton)
+//        homeButton.setOnClickListener {
+//            findNavController().navigate(R.id.action)
+//        }
+        return view
+    }
+
+}
---
+++ app/src/main/java/com/example/moveit/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.moveit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.navigateUp
+
+class MainActivity : AppCompatActivity() {
+
+    //nav controller
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment) as NavHostFragment
+
+        navController =navHostFragment.navController
+    }
+
+
+}
